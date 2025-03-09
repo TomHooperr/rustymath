a7c87c1ec2b8c26451e3ea2fecb9d00e81106737
Tom H <thooper257@gmail.com>
2025-03-09 13:37:13 +1030

    Added basic arithmetic functions

--- src/core/arithmetic.rs
+++ src/core/arithmetic.rs
@@ -1,30 +1,55 @@
 
-pub fn sum(a: f64, b: f64) -> f64 {
+pub fn sum_f64(a: f64, b: f64) -> f64 {
     a + b
 }
 
-pub fn subtract(a: f64, b: f64) -> f64 {
+pub fn sum_i32(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn subtract_f64(a: f64, b: f64) -> f64 {
     a - b
 }
 
-pub fn multiply(a: f64, b: f64) -> f64 {
+pub fn subtract_i32(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+pub fn multiply_f64(a: f64, b: f64) -> f64 {
     a * b
 }
 
-pub fn divide(a: f64, b: f64) -> f64 {
+pub fn multiply_i32(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+pub fn divide_f64(a: f64, b: f64) -> f64 {
     a / b
 }
 
-pub fn modulo(a: f64, b: f64) -> f64 {
+pub fn divide_i32(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+pub fn modulo_f64(a: f64, b: f64) -> f64 {
     let rem = a % b;
     if rem < 0.0 {
-        sum(rem, b)
+        rem + b
     } else {
         rem
     }
 }
 
-pub fn abs(a: f64) -> f64 {
+pub fn modulo_i32(a: i32, b: i32) -> i32 {
+    let rem = a % b;
+    if rem < 0 {
+        rem + b
+    } else {
+        rem
+    }
+}
+
+pub fn abs_f64(a: f64) -> f64 {
     if a < 0.0 {
         -a
     } else {
@@ -32,22 +57,58 @@ pub fn abs(a: f64) -> f64 {
     }
 }
 
-pub fn power(a: f64, b: f64) -> f64 {
+pub fn abs_i32(a: i32) -> i32 {
+    abs_f64(a as f64) as i32
+}
+
+pub fn power_f64(a: f64, b: f64) -> f64 {
     let mut base = a;
-    let mut exp = abs(b);
+    let mut exp = abs_f64(b);
     let mut res = 1.0;
 
+    if b == 0.0 {
+        return 1.0;
+    }
+
     while exp > 0.0 {
-        if modulo(exp, 2.0) >= 1.0 {
-            res = multiply(res, base);
+        if modulo_f64(exp, 2.0) >= 1.0 {
+            res = res * base;
         }
 
-        base = multiply(base , base);
-        exp = divide(exp, 2.0);
+        exp = exp / 2.0;
+        base = base * base;
     }
 
     if b < 0.0 {
-        divide(1.0, res)
+        1.0 / res
+    } else {
+        res
+    }
+}
+
+pub fn power_i32(a: i32, b: i32) -> i32 {
+    let mut base = a;
+    let mut exp = abs_i32(b);
+    let mut res = 1;
+
+    if b == 0 {
+        return 1;
+    }
+
+    while exp > 1 {
+        if modulo_i32(exp, 2) > 0 {
+            res = res * base;
+            exp = exp - 1;
+        }
+
+        exp = exp / 2;
+        base = base * base;
+    }
+
+    res = res * base;
+
+    if b < 0 {
+        1 / res
     } else {
         res
     }
